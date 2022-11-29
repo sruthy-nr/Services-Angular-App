980dffa0874f418356fc8ded10ee131bbb471622
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-29 22:07:21 +0530

    publicapi

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,4 +10,7 @@ export class ApiService {
   fetchPassenger=()=>{
     return this.http.get("https://api.instantwebtools.net/v1/passenger?page=0&size=30.")
   }
+  fetchUsPublic=()=>{
+    return this.http.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+  }
 }
